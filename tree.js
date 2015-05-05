335e979701cb0abe797d90ade7e8be7332c9b41e
Matt Conrad <mmconrad@gmail.com>
2015-05-05 15:21:52 -0700

    Adds noChildren function

--- tree.js
+++ tree.js
@@ -48,3 +48,22 @@ var tree = {  name: "Nancy",
               ]
             };
 
+
+// fucntion that prints the name of all people with no children
+function noChildren(node) {
+
+  if (node.children.length < 1) {
+    console.log(node.name);
+    return node.name;     // return is not important as output for the prompt, but is needed to cut off recursive calls
+  } 
+  // console.log('now we are at', node);
+  for (var i = 0; i < node.children.length; i++) {
+    noChildren(node.children[i]);
+  }
+}
+
+noChildren(tree);
+
+
+
+
