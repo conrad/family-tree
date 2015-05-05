12be9adcf3112f68d146bc779b311be0275d1c9e
Matt Conrad <mmconrad@gmail.com>
2015-05-05 15:16:24 -0700

    Initial commit and adds family tree data structure

---
+++ tree.js
@@ -0,0 +1,50 @@
+
+var tree = {  name: "Nancy",
+              children: [ 
+                { name: "Adam",
+                  children: [] 
+                },
+                { name: "Jill",
+                  children: [ 
+                    { name: "Kevin",
+                      children: [ 
+                        { name: "Samuel",
+                          children: []
+                        },
+                        { name: "George",
+                          children: [
+                            { name: "Robert",
+                              children: []
+                            },
+                            { name: "Patrick",
+                              children: []
+                            } 
+                          ]
+                        },
+                        { name: "James",
+                          children: [
+                            { name: "Mary",
+                              children: []
+                            }
+                          ]
+                        },
+                        { name: "Aaron",
+                          children: []
+                        }
+                      ]
+                    }
+                  ]
+                },
+                { name: "Carl",
+                  children: [
+                    { name: "Catherine",
+                      children: []
+                    },
+                    { name: "Joseph",
+                      children: []
+                    }
+                  ]
+                }
+              ]
+            };
+
